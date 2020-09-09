784d764b3fac8548913275e39ff8dadcce15aa94
JValtteri <46611993+JValtteri@users.noreply.github.com>
2020-09-09 15:22:50 +0300

    Changed no data behavior
    - no data no longer defaults to zero.
    - now instead the default is None.
    - web handles the missing data points, not logger
    
    Bandaid fix for crashing, when db is locked.
    - Logger retries once after a 2 second timeout.
    - If the db is locked again, results in a crash again.

--- ruuvitag-web.py
+++ ruuvitag-web.py
@@ -94,7 +94,8 @@ def index():
 				borderColor: 'rgba{}',
 				pointRadius: 0,
 				fill: false,
-		        lineTension: 0.2,
+				spanGaps: false,
+		        lineTension: 0,
 				data: {}
 			}}"""
 			for sensor in sensors:
